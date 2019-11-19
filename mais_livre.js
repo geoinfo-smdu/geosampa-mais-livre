c8fd1123c213ededa8d4a3484eb058e0b59a7cfa
Fernando Gomes <feromes@gmail.com>
2019-11-19 13:56:48 -0300

    Resolvendo o Load da página

--- mais_livre.js
+++ mais_livre.js
@@ -1,10 +1,3 @@
-/* setTimeout(function() { 
-    document.body.style.border = "5px solid red";
-    document.evaluate('/html/body/form/div[22]/table/tbody/tr/td[1]/img', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.src = "seila";
-    //document.getElementById("Cabecalho").innerHTML = "<p>teste</p>";
-    console.log("seila");
-    }, 2000); */
-
 function verifyHeaderElement() {
     return document.evaluate('/html/body/form/div[22]/table/tbody/tr/td[1]/img', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue == null;
 }
@@ -23,19 +16,6 @@ var updateHeader = function() {
 
 updateHeader();
 
-//setInterval(function(){console.log('seila');}, 2500); 
-
-/*
-while (document.evaluate('/html/body/form/div[22]/table/tbody/tr/td[1]/img', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue !== null) {
-    //document.body.style.border = "5px solid red";
-    setTimeout(
-        function() {
-            console.log('100');
-        },
-        100);
-    );
-};
-*/
  
 
 
