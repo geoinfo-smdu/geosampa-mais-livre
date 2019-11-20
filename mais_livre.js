92be81589c128297bc0221b923d23989a3035340
feromes <feromes@gmail.com>
2019-11-19 22:47:51 -0200

    Funcionando a alteração do logo

--- mais_livre.js
+++ mais_livre.js
@@ -5,12 +5,11 @@ function verifyHeaderElement() {
 console.log(verifyHeaderElement());
 
 var updateHeader = function() {
-    console.log('seila');
     if (verifyHeaderElement()) {
         setTimeout(updateHeader, 100);
     } 
     else {
-        console.log('passou!!!');
+        document.evaluate('/html/body/form/div[22]/table/tbody/tr/td[1]/img', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue.src =  browser.runtime.getURL("images/logo_superior_esquerda.png");
     }
 }
 
