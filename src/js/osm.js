fcdd105c17101352d0fca57801eacfa3d4cce373
yubathom <yubathom@gmail.com>
2020-02-18 00:56:23 -0300

    feat(wip): busca por CEP com nominatim do osm

--- src/js/osm.js
+++ src/js/osm.js
@@ -1,47 +1,83 @@
 import axios from 'axios'
-(function () {
+import MaskInput from 'mask-input'
 
+(function () {
     const panelid = 'OpenLayers_Control_Panel_581'
     const element = {
-        init: id => window.id = id,
-        get: () => document.getElementById(window.id),
+        init: id => document.id = id,
+        get: () => document.getElementById(document.id),
         create: (node, el) => {
             let child = document.createElement(el)
+            const randomId = `osm__${Math.floor(Math.random() * 9999) + 1}`
+
             node.appendChild(child)
             child.setAttribute("class", `osm olControlPanel__custom-item`)
-            return child
+            child.setAttribute("id", randomId)
+
+            return { child, randomId }
         },
         render: (node, template) => {
             if (!node) return
             node.innerHTML = template
-        },
-        onclick: (query, callback) => {
-            document.querySelector(query).addEventListener('click', callback)
         }
+    }
+    const icons = {
+        seta: `
+            <svg width="16" height="8" viewBox="0 0 16 8" fill="none">
+                <path d="M12.01 3H0V5H12.01V8L16 4L12.01 0V3Z" fill="white"/>
+            </svg>
+        `
     }
 
     const view = id => {
         if (!id) { return }
         const panel = Object.create(element)
-        panel.init('OpenLayers_Control_Panel_581')
+        panel.init(panelid)
 
         const olmenu = panel.get()
-        const menuItem = panel.create(olmenu, 'form')
+        const menuItem = panel.create(olmenu, 'div')
 
-        panel.render(menuItem, `
-            <label for="osm__cep">CEP</label>
-            <input id="osm__cep" / type="text"></input>
-            <button class="osm__buscar" id="osm__buscar">buscar por cep</button>
+        panel.render(menuItem.child, `
+            <form>
+                <div class="osm__group" id="osm__group--cep">
+                    <label class="osm__label" for="osm__cep">CEP</label>
+                    <input class="osm__input" id="osm__cep type="number" value></input>
+                    <button class="osm__btn" id="osm__buscar">BUSCAR</button>
+                </div>
+                <button class="osm__toggle" id="osm__toggle-cep">
+                    <span class="toggle__span ativo">${icons.seta}</span>
+                </button>
+            </form>
         `)
 
-        panel.onclick('#osm__buscar', event => {
-            event.preventDefault()
-            map.setCenter([7391311.26420, 324625.01921353], 4, true, false)
+        jQuery('#osm__toggle-cep').click(() => {
+            jQuery(`#${menuItem.randomId}`).toggleClass('aberto')
+            jQuery('.toggle__span').toggleClass('ativo')
 
-            axios.get(`https://nominatim.openstreetmap.org/search?city=S%C3%A3o%20Paulo&postalcode=01307-000&format=json`)
-                .then(res=>console.log(res))
-
+            new MaskInput(document.querySelector('.osm__input'), {
+                mask: '00000-000',
+                alwaysShowMask: true,
+                maskChar: '0'
+            })
+            document.querySelector('.osm__input').focus()
         })
+
+        jQuery('#osm__buscar').click(event => {
+            event.preventDefault()
+
+            const cep = jQuery('.osm__input').val()
+            // map.setCenter([7391311.26420, 324625.01921353], 4, true, false)
+            axios.get(`https://nominatim.openstreetmap.org/search?city=S%C3%A3o%20Paulo&postalcode=${cep}&format=json`)
+                .then(res => {
+                    const lat = res.data[0].lat
+                    const lon = res.data[0].lon
+                    map.setCenter([lat, lon], 8, true, false)
+                })
+                .catch(err => {
+                    console.error(err)
+                })
+        })
+
     }
 
     const osm = () => {
